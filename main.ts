8b9a58ce1d999627e96a532d28b3728b24cb02f4
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-15 11:03:20 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+for (let index = 0; index <= 9; index++) {
+    basic.showNumber(9 - index)
+    basic.pause(500)
+}
+while (true) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(200)
+}
